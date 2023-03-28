87010aacf65f76bdd4e381a35e2b40664050da23
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-03-28 15:34:53 +0300

    feat: add inputs

--- src/pages/drawCalculatorPage.ts
+++ src/pages/drawCalculatorPage.ts
@@ -1,5 +1,12 @@
-const drawCalculatorPage = () =>
-  `
+import config from '../data/config.json';
+// import data from '../data/data.json';
+
+const drawCalculatorPage = () => {
+  // const materialArray = data.filter((item) => item.type === 'list');
+  const strengthArray = config.filter((item) => item.type === 'frame');
+  const widthConfig = config.find((item) => item.type === 'size' && item.key === 'width');
+  const heightConfig = config.find((item) => item.type === 'size' && item.key === 'height');
+  return `
   <main class="main">
     <h1>Frame calculator</h1>
     <div class="wrapper">
@@ -9,23 +16,23 @@ const drawCalculatorPage = () =>
         </div>
         <div class="width">
           <h4 class="width-header">Choose width</h4>
-          <input type="range" class="width-range" />
+          <input type="range" class="width-range" min=${widthConfig?.min} max=${widthConfig?.max} />
         </div>
         <div class="height">
           <h4 class="height-header">Choose height</h4>
-          <input type="range" class="height-range" />
+          <input type="range" class="height-range" min=${heightConfig?.min} max=${heightConfig?.max} />
         </div>
         <div class="strength">
           <h4 class="strength-header">Choose strength</h4>
-          <label>
-            <input type="radio" class="radio" value="light" />
-          </label>
-          <label>
-            <input type="radio" class="radio" value="standard" />
-          </label>
-          <label>
-            <input type="radio" class="radio" value="strong" />
-          </label>
+          ${strengthArray
+            .map(
+              (item) =>
+                `<label>
+              ${item.key}
+              <input type="radio" class="radio" value=${item.key} />
+            </label>`
+            )
+            .join('')}
         </div>
         <button class="button calculate-button">Calculate</button>
       </section>
@@ -36,5 +43,6 @@ const drawCalculatorPage = () =>
     </div>
   </main>
 `;
+};
 
 export default drawCalculatorPage;
